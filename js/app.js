79b8f0451a26b6ab4cdcd85cbab3652c6676fa58
Nick Mullaney <nick.mullaney@gmail.com>
2022-10-19 04:32:46 +0000

    now with bio and hopefully better js

--- js/app.js
+++ js/app.js
@@ -1,54 +1,58 @@
 
-// function topping(){
-//   let answer1 = prompt('Do you want pepperoni on your pizza?');
-//   console.log(answer1);
-// }
-let answer1 = prompt('Do you want Pepperoni on your Pizza?');
-console.log(answer1);
-let answer2 = prompt('Do you want anchovies?');
-console.log(answer2);
+// let answer1 = prompt('Do you want Pepperoni on your Pizza?');
+// console.log(answer1);
+// let answer2 = prompt('Do you want anchovies?');
+// console.log(answer2);
 
-//String concatenation
-alert('You answered ' + answer1 + ' to the pepperoni question');
+// //String concatenation
+// alert('You answered ' + answer1 + ' to the pepperoni question');
     
-if(answer2 === 'yes' || answer2 === 'YES' || answer2 === 'Yes'){
-  alert('Why do you like anchovies?!?');
-}
-let size = prompt('What size pizza do you want? small, medium or large.');
+// if(answer2 === 'yes' || answer2 === 'YES' || answer2 === 'Yes'){
+//   alert('Why do you like anchovies?!?');
+// }
+// let size = prompt('What size pizza do you want? small, medium or large.');
 
-if(size === 'large' || size === 'Large'){
-  alert('Large pizza on the way');
-}
-else if(size === 'medium' || size === 'Medium'){
-  alert('Medium pizza on the way');
-}
-else if(size === 'small' || size === 'small'){
-  alert('Small pizza on the way');
-}
-else{
-  alert('You types something other than small, medium or large');
-}
+// if(size === 'large' || size === 'Large'){
+//   alert('Large pizza on the way');
+// }
+// else if(size === 'medium' || size === 'Medium'){
+//   alert('Medium pizza on the way');
+// }
+// else if(size === 'small' || size === 'small'){
+//   alert('Small pizza on the way');
+// }
+// else{
+//   alert('You types something other than small, medium or large');
+// }
 
-if(size === 'large' || size === 'medium'){
-  alert('You ordered a large or medium pizza');
-}
+// if(size === 'large' || size === 'medium'){
+//   alert('You ordered a large or medium pizza');
+// }
 
-let order = '';
+// let order = '';
 
-if(size === 'large' || size === 'medium' || size === 'small'){
-  order = 'You want a ' + size + ' sized pizza';
-}else{
-  order = 'You want a ??? sized pizza';
-}
+// if(size === 'large' || size === 'medium' || size === 'small'){
+//   order = 'You want a ' + size + ' sized pizza';
+// }else{
+//   order = 'You want a ??? sized pizza';
+// }
 
-if(answer1 === 'yes'){
-  order = order + ' with pepperoni.';
-}
-if(answer2 === 'yes'){
-  order = order + ' with anchovies, (gross)';
-}
+// if(answer1 === 'yes'){
+//   order = order + ' with pepperoni.';
+// }
+// if(answer2 === 'yes'){
+//   order = order + ' with anchovies, (gross)';
+// }
 
-alert(order);
+// alert(order);
+
+let userName = yourName();
+let spice = spiceYouLike();
+alert('I also like ' + spice + ' ' + userName + '!');
+let visit = userVisits();
+alert(visit + ' sounds like a great place to visit ' + userName + '.');
+let newSpiceBlend = newSpice();
+alert('Thanks for the suggestion ' + userName + '. We love thinking of new combinations and a ' + newSpiceBlend + ' blend, with hints of ' + spice + ' and ingredients from ' + visit + ' would be great!');
 
 function yourName(){
   let valid = false;
