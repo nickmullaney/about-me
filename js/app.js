51f358733bbedc1de8e42c9e88a45ac9586f0057
Nick Mullaney <nick.mullaney@gmail.com>
2022-10-20 03:24:45 +0000

    now with quiz and a fully separated JS doc

--- js/app.js
+++ js/app.js
@@ -1,125 +1,88 @@
-
-// let answer1 = prompt('Do you want Pepperoni on your Pizza?');
-// console.log(answer1);
-// let answer2 = prompt('Do you want anchovies?');
-// console.log(answer2);
-
-// //String concatenation
-// alert('You answered ' + answer1 + ' to the pepperoni question');
-    
-// if(answer2 === 'yes' || answer2 === 'YES' || answer2 === 'Yes'){
-//   alert('Why do you like anchovies?!?');
-// }
-// let size = prompt('What size pizza do you want? small, medium or large.');
-
-// if(size === 'large' || size === 'Large'){
-//   alert('Large pizza on the way');
-// }
-// else if(size === 'medium' || size === 'Medium'){
-//   alert('Medium pizza on the way');
-// }
-// else if(size === 'small' || size === 'small'){
-//   alert('Small pizza on the way');
-// }
-// else{
-//   alert('You types something other than small, medium or large');
-// }
-
-// if(size === 'large' || size === 'medium'){
-//   alert('You ordered a large or medium pizza');
-// }
-
-// let order = '';
-
-// if(size === 'large' || size === 'medium' || size === 'small'){
-//   order = 'You want a ' + size + ' sized pizza';
-// }else{
-//   order = 'You want a ??? sized pizza';
-// }
-
-// if(answer1 === 'yes'){
-//   order = order + ' with pepperoni.';
-// }
-// if(answer2 === 'yes'){
-//   order = order + ' with anchovies, (gross)';
-// }
-
-// alert(order);
+let userName = prompt('What is your name?');
+console.log('user name',userName);
+alert('Welcome to my website ' + userName + ', It\'s so nice to have you here. I\'d love to ask you a few questions. Please answer Yes or No to the following questions.');
 
 
-// New questions for this trial
-
-//let userName = yourName();
-//let spice = spiceYouLike();
-
-
-function yourName(){
-  let valid = false;
-  while (valid === false){
-    let userName = prompt('What is your name?');
-    document.write (userName);
-    console.log('user name',userName);
-    if(userName === null){
-      valid = false;
-      yourName();
-    }
-    else{
-      valid = true;
-      return userName;
-    }
-  }
+let count = 0;
+let answer1 = prompt('Do you think I\'ve lived in more than 6 states? Yes or No or y/n.');
+console.log('Q1 Answer: ',answer1);
+answer1 = answer1.toLowerCase();
+if(answer1 === 'yes' || answer1 === 'y'){
+  alert ('Thats correct! I\'ve lived in 7 States. Florida, Illinois, Maryland, New York, Rhode Island, Boston & Washinton.');
+  count = count + 1;
+}
+else if(answer1 === 'no' || answer1 === 'n'){
+  alert('I\'ve lived in 7 States. Florida, Illinois, Maryland, New York, Rhode Island, Boston & Washinton.');
+}
+else {
+  alert('Thats not a valid answer.');
 }
 
-function goodbye(userName){
-  document.write(userName);
-  console.log('user name',userName);
+let answer2 = prompt('Do you think I\'ve ever skydived before? Yes or No or y/n.');
+console.log('Q2 Answer: ',answer2);
+answer2 = answer2.toLowerCase();
+if(answer2 === 'yes' || answer2 === 'y'){
+  alert ('Absolutely not that\'s terrifying.');
+}
+else if(answer2 === 'no' || answer2 === 'n'){
+  alert('I see you also like two feet on the ground. Good choice');
+  count = count + 1;
+}
+else {
+  alert('Thats not a valid answer.');
 }
 
-function spiceYouLike(){
-  let spice = prompt('Which of the 4 flavor profiles do you like most? Sweet, Savory, Spicy, Fragrant');
-  spice = spice.toLowerCase();
-  console.log('spice',spice);
-  if (spice === 'sweet' || spice === 'swt'){
-    alert ('If you like ' + spice + ', you will love our Valhalla Blend');
-  }
-  else if(spice === 'savory' || spice === 'sav'){
-    alert ('If you like ' + spice + ', you will love our Pangea Blend');
-  }
-  else if(spice === 'spicy' || spice === 'spy'){
-    alert ('If you like ' + spice + ', you will love our El Dorado Blend');
-  }
-  else if(spice === 'fragrant' || spice === 'Frgt'){
-    alert ('If you like Nick' + spice + ', you will love our Atlantis Blend');
-  }
-  else{
-    alert('I didn\'t quite catch that? Explore around to find what you like');
-  }
-  return spice;
+let answer3 = prompt('Do you think I\'ve ever been published? Yes or No or y/n.');
+console.log('Q3 Answer: ',answer3);
+answer3 = answer3.toLowerCase();
+if(answer3 === 'yes' || answer3 === 'y'){
+  alert ('I am currently working on it, but I\'m not published yet, I\'ll let you know when my fantasy novel is complete and up.');
+}
+else if(answer3 === 'no' || answer3 === 'n'){
+  alert('Good Guess! I am currently working on it, but I\'m not published yet, I\'ll let you know when my fantasy novel is complete and up.');
+  count = count + 1;
+}
+else {
+  alert('Thats not a valid answer.');
 }
 
-function userVisits(){
-  let visit = prompt('If you could visit any location, where would it be?');
-  console.log('Where to visit: ', visit);
-  return visit;
+let answer4 = prompt('Have I ever taken coding classes before? Yes or No or y/n.');
+console.log('Q4 Answer: ',answer4);
+answer4 = answer4.toLowerCase();
+if(answer4 === 'yes' || answer4 === 'y'){
+  alert ('I actually have, I took a few classes at South Seattle College during the pandemic. Great guess!');
+  count = count + 1;
+}
+else if(answer4 === 'no' || answer4 === 'n'){
+  alert('Good guess, but I actually have, I took a few classes at South Seattle College during the pandemic.');
+}
+else {
+  alert('Thats not a valid answer.');
 }
 
-function newSpice(){
-  let newSpice = prompt('What kind of new spice blend would you like to see? Something Zesty, Fruity, Fragrant, etc.');
-  console.log('new spice blend adjective: ',newSpice);
-  return newSpice;
+let answer5 = prompt('Do you think I got married by the water? Yes or No or y/n.');
+console.log('Q5 Answer: ',answer5);
+answer5 = answer5.toLowerCase();
+if(answer5 === 'yes' || answer5 === 'y'){
+  alert ('I have almost always lived by the water but I actually got married at the base of Mt Tahoma(Mt Rainier) in the woods.');
+}
+else if(answer5 === 'no' || answer5 === 'n'){
+  alert('Great guess, I got married at the base of Mt Tahoma(Mt Rainier) in the woods.');
+  count = count + 1;
+}
+else {
+  alert('Thats not a valid answer.');
 }
 
-function spiceAlert(){
-  alert('I also like ' + spice + ' ' + userName + '!');
+if(count >= 4){
+  alert('Great job on the Quiz! You got ' +count+ '/5!');
 }
-// let visit = userVisits();
-
-function placeAlert(){
-  alert(visit + ' sounds like a great place to visit ' + userName + '.');
+else if (count === 3){
+  alert('Nice job on the Quiz, you got ' +count+ '/5!');
 }
-
-// let newSpiceBlend = newSpice();
-
-function fullAlert(){ 
-alert('Thanks for the suggestion ' + userName + '. We love thinking of new combinations and a ' + newSpiceBlend + ' blend, with hints of ' + spice + ' and ingredients from ' + visit + ' would be great!');
+else if (count === 1 || count === 2){
+  alert('You didn\'t do so well on the Quiz, you got ' +count+ '/5.');
+}
+else{
+  alert('Well a ' +count+ '/5, I\'m not even mad, I\'m impressed');
 }
