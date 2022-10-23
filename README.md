0eb1c7acf546922b46176124b936a591d3466ac2
Nick Mullaney <nick.mullaney@gmail.com>
2022-10-23 01:10:20 +0000

    now with more java, mmm coffee

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # about-me
+
+About me now contains the following features
+
+* About me Guessing Game
+* New add on to guessing game for top 10
+* information about me
+* Biography
+* Work experience
+* Education
+* Fun Duck Gif
+* included loop functions for processing user answers
+* 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 'use strict';
+
+let count = 0;
+
+function askAgain(entry){
+  while(!entry){
+    entry = prompt('Let\'s try that again? Shall we?');
+  }
+  return entry;
+}
+
 let userName = prompt('What is your name?');
+askAgain(userName);
 console.log('user name',userName);
 alert('Welcome to my website ' + userName + ', It\'s so nice to have you here. I\'d love to ask you a few questions. Please answer Yes or No to the following questions.');
 
 
-let count = 0;
+
 let answer1 = prompt('Do you think I\'ve lived in more than 6 states? Yes or No or y/n.');
+answer1 = askAgain(answer1);
+
 console.log('Q1 Answer: ', answer1);
 answer1 = answer1.toLowerCase();
 if(answer1 === 'yes' || answer1 === 'y'){
@@ -20,6 +33,7 @@ else {
 }
 
 let answer2 = prompt('Do you think I\'ve ever skydived before? Yes or No or y/n.');
+answer2 = askAgain(answer2);
 console.log('Q2 Answer: ', answer2);
 answer2 = answer2.toLowerCase();
 if(answer2 === 'yes' || answer2 === 'y'){
@@ -34,6 +48,7 @@ else {
 }
 
 let answer3 = prompt('Do you think I\'ve ever been published? Yes or No or y/n.');
+answer3 = askAgain(answer3);
 console.log('Q3 Answer: ',answer3);
 answer3 = answer3.toLowerCase();
 if(answer3 === 'yes' || answer3 === 'y'){
@@ -48,6 +63,7 @@ else {
 }
 
 let answer4 = prompt('Have I ever taken coding classes before? Yes or No or y/n.');
+answer4 = askAgain(answer4);
 console.log('Q4 Answer: ',answer4);
 answer4 = answer4.toLowerCase();
 if(answer4 === 'yes' || answer4 === 'y'){
@@ -62,6 +78,7 @@ else {
 }
 
 let answer5 = prompt('Do you think I got married by the water? Yes or No or y/n.');
+answer5 = askAgain(answer5);
 console.log('Q5 Answer: ',answer5);
 answer5 = answer5.toLowerCase();
 if(answer5 === 'yes' || answer5 === 'y'){
@@ -75,18 +92,40 @@ else {
   alert('Thats not a valid answer.');
 }
 
+// Look at this and think about a while loop or why this one isn't scanning through. 
+//
+let answer6 = 4;
+for(let i = 0; i < 4; i++){
+  let guess6 = prompt('I know you are getting tired of this, but we only have one more question left. I need you to guess a number between 1 and 10. You get 4 guesses to get it right.');
+  guess6 = askAgain(guess6);
+  if (guess6 < answer6){
+    alert('You guessed a little low. Try again');
+  }
+  else if(guess6 > answer6){
+    alert('You guessed a little high. Try again');
+  }
+  else{
+    alert(answer6 + ' was correct. Congrats you guessed it correctly!');
+    count = count + 1;
+    break;
+  }
+}
+
+
+//Count tally
 if(count >= 4){
-  alert('Great job on the Quiz ' + userName + '! You got ' +count+ '/5!');
+  alert('Great job on the Quiz ' + userName + '! You got ' +count+ '/6!');
 }
 else if (count === 3){
-  alert('Nice job on the Quiz ' + userName + ', you got ' +count+ '/5!');
+  alert('Nice job on the Quiz ' + userName + ', you got ' +count+ '/6!');
 }
 else if (count === 1 || count === 2){
-  alert('You didn\'t do so well on the Quiz ' + userName + ', you got ' +count+ '/5.');
+  alert('You didn\'t do so well on the Quiz ' + userName + ', you got ' +count+ '/6.');
 }
 else{
-  alert('Well a ' +count+ '/5, I\'m not even mad ' + userName + '!, I\'m impressed');
+  alert('Well a ' +count+ '/6, I\'m not even mad ' + userName + '!, I\'m impressed');
 }
+
 
 // Stack Overflow helped with building this
 const audio = new Audio('https://www.fesliyanstudios.com/play-mp3/7008');
